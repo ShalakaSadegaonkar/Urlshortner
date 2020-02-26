abe689740a7fa40cebb14b64b344215f8ff6a6cc
ssadegaonkar@parkar.consulting <ssadegaonkar@parkar.consulting>
2020-02-26 20:32:56 +0530

    initial commit

---
+++ helloWorld.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 25 23:04:47 2020
+
+@author: Shalaka.Sadegaonkar
+"""
+
+
+from flask import Flask 
+app = Flask(__name__)
+
+@app.route('/') 
+def index(): 
+   return '<h1> Hello world</h1>'
+if __name__ == '__main__': 
+    app.run(debug=True)
+    
+    
