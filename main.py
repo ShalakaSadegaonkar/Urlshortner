51846b4e3331984ea9795ceb87b088079c3bd83f
ShalakaSadegaonkar <ssadegaonkar@parkar.consulting>
2020-03-12 18:57:49 +0530

    Changes in host name

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from app import app,db
 import url.views
 import url.models
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
